5d3c4fa49c7f9a3499b71551122b191c904c51e9
cleandsk <54430729+cleandsk@users.noreply.github.com>
2024-09-04 12:48:19 +0100

    update

---
+++ checkpoint/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import PlayerList from './PlayerList';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>FIFA PLAYER CARDS</h2>
+      <PlayerList />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ checkpoint/src/players.js
@@ -0,0 +1,44 @@
+import img1 from './assets/messi.jpeg'
+import img2 from './assets/victor.jpeg'
+import img3 from './assets/tevez.jpeg'
+import img4 from './assets/onana.jpeg'
+
+const players = [
+    {
+        name: "Lionel Messi",
+        team: "Kano Pillars",
+        nationality: "Nigeria",
+        jerseyNumber: 10,
+        age: 40,
+        image: img1
+    },
+
+    {
+        name: "Victor Osimen",
+        team: "Kano Pillars",
+        nationality: "Nigeria",
+        jerseyNumber: 9,
+        age: 40,
+        image: img2
+    },
+
+    {
+        name: "Tevez",
+        team: "Kano Pillars",
+        nationality: "Nigeria",
+        jerseyNumber: 7,
+        age: 40,
+        image: img3
+    },
+
+    {
+        name: "Onana",
+        team: "Kano Pillars",
+        nationality: "Nigeria",
+        jerseyNumber: 1,
+        age: 27,
+        image: img4
+    },
+]
+
+export default players
